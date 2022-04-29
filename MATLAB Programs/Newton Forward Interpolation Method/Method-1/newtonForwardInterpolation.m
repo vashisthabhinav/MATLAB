0a91a5080ad8887081c2c8dc23d4018562ab1537
Abhinav Vashisth <89785800+vashisthabhinav@users.noreply.github.com>
2022-04-29 07:18:49 +0530

    Add files via upload

--- MATLAB Programs/Newton Forward Interpolation Method/Method-1/newtonForwardInterpolation.m
+++ MATLAB Programs/Newton Forward Interpolation Method/Method-1/newtonForwardInterpolation.m
@@ -1,6 +1,11 @@
+%% Newton's Forward Interpolation Method
+% Submitted by- Abhinav Vashisth
+% Roll No.- 20bch010
+
 X = input('Enter list of abscissas: ');
 Y = input('Enter list of ordinates: ');
 p0 = input('Enter point of approximations: ');
+
 n = length(X);
 h = X(2)-X(1);
 F = zeros(n,n);
@@ -23,8 +28,3 @@ C
 
 A = polyval(C,p0);
 fprintf('Approximate value at given data point is: %.4f\n',A);
-x = linspace(X(1),X(n),100);
-y = polyval(C,x);
-plot(x,y,'r')
-hold on
-plot(X,Y,'b')
